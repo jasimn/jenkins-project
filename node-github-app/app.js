b3d1526f0f540f0e53e995acb98c5e09c22ec769
jasimn <113772434+jasimn@users.noreply.github.com>
2025-12-24 16:47:23 +0530

    Create app.js

---
+++ node-github-app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Jenkins Pipeline with Node.js from GitHub!");
+});
+
+const PORT = 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`App running on port ${PORT}`);
+});
